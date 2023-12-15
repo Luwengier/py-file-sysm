3ce905bba3adbf5fd90254b17c6558e5e1c9595f
Luewngier <g217881111@gmail.com>
2023-12-15 23:45:25 +0800

    Generate results letters according to name list

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 with open("./Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
 
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_file.read().replace("[name]", stripped_name)
+
+        with open(
+            f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w"
+        ) as completed_letter:
+            completed_letter.write(new_letter)
+
+        letter_file.seek(0)
+#         with open(
+#             f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w"
+#         ) as completed_letter:
+#             completed_letter.write(new_letter)
 
 # TODO: Create a letter using starting_letter.txt
 # for each name in invited_names.txt
